b42ba00144bc8b5419e11d6b55dfddd15776cdcc
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-03-18 22:23:14 -0300

    build: check if email is valid

--- src/login_page.py
+++ src/login_page.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from backend import authenticate_user
-import re
 
-def is_valid_email(email):
-    # Expressão regular para validar email
-    regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    return re.match(regex, email) is not None
 
 def run_login_page() -> str:
     st.title("Login Page")
@@ -14,11 +9,8 @@ def run_login_page() -> str:
     username = st.text_input("Username:")
     email = st.text_input("Email:")
     password = st.text_input("Password:", type="password")
-    
     if st.button("Login"):
-        if not is_valid_email(email):
-            st.error("Invalid email format")
-        elif authenticate_user(username, password):
+        if authenticate_user(username, password):
             st.success("Login successful!")
             return username
         else:
--- src/register_page.py
+++ src/register_page.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import re
 from backend import create_new_user
+
+def is_valid_email(email):
+    # Expressão regular para validar email
+    regex = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+    return re.match(regex, email) is not None
 
 def run_register_page():
     st.title("Registration Page")
@@ -8,7 +14,11 @@ def run_register_page():
     new_username = st.text_input("New Username:", key="new_username_input")
     new_email = st.text_input("New Email:", key="new_email_input")
     new_password = st.text_input("New Password:", type="password", key="new_password_input")
+    
     if st.button("Register New User"):
-        create_new_user(new_username,new_email ,new_password)
-        st.success("New user registered successfully!")
-        return new_username  # Return the registered username
+        if not is_valid_email(new_email):
+            st.error("Invalid email format")
+        else:
+            create_new_user(new_username, new_email, new_password)
+            st.success("New user registered successfully!")
+            return new_username  # Return the registered username
