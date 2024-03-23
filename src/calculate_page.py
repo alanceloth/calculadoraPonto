e5ff60cecdd9ad396b75c3b37f861bbb72b6ad2f
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-03-22 23:53:27 -0300

    fix: authenticate_google_calendar env var

--- src/calculate_page.py
+++ src/calculate_page.py
@@ -16,28 +16,33 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 def authenticate_google_calendar():
     creds = None
+    # Check if token.pickle file exists
     if os.path.exists('token.pickle'):
+        # Load credentials from token file
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+    # If credentials are missing, expired, or invalid, try to refresh or load from environment variables
     if not creds or not creds.valid:
+        # If credentials exist and are expired, refresh them
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            # Modificado para carregar as credenciais da conta de serviço do Streamlit Cloud Secrets
+            # Load credentials from environment variables
             service_account_info = {
-              "type": "service_account",
-              "project_id": os.environ["GOOGLE_PROJECT_ID"],
-              "private_key_id": os.environ["GOOGLE_PRIVATE_KEY_ID"],
-              "private_key": os.environ["GOOGLE_PRIVATE_KEY"],
-              "client_email": os.environ["GOOGLE_CLIENT_EMAIL"],
-              "client_id": os.environ["GOOGLE_CLIENT_ID"],
-              "auth_uri": os.environ["GOOGLE_AUTH_URI"],
-              "token_uri": os.environ["GOOGLE_TOKEN_URI"],
-              "auth_provider_x509_cert_url": os.environ["GOOGLE_AUTH_PROVIDER_CERT_URL"],
-              "client_x509_cert_url": os.environ["GOOGLE_CLIENT_CERT_URL"]
+                "type": os.environ.get("GOOGLE_TYPE"),
+                "project_id": os.environ.get("GOOGLE_PROJECT_ID"),
+                "private_key_id": os.environ.get("GOOGLE_PRIVATE_KEY_ID"),
+                "private_key": os.environ.get("GOOGLE_PRIVATE_KEY").replace('\\n', '\n'),
+                "client_email": os.environ.get("GOOGLE_CLIENT_EMAIL"),
+                "client_id": os.environ.get("GOOGLE_CLIENT_ID"),
+                "auth_uri": os.environ.get("GOOGLE_AUTH_URI"),
+                "token_uri": os.environ.get("GOOGLE_TOKEN_URI"),
+                "auth_provider_x509_cert_url": os.environ.get("GOOGLE_AUTH_PROVIDER_CERT_URL"),
+                "client_x509_cert_url": os.environ.get("GOOGLE_CLIENT_CERT_URL")
             }
-            creds = Credentials.from_service_account_info(
-                service_account_info, scopes=SCOPES)
+            # Create credentials from service account info
+            creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+    # Return Google Calendar API service
     return build('calendar', 'v3', credentials=creds)
 
 
