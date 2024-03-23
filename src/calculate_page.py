050b9054d7e90853eff2b38526bad92842604754
Alan Lanceloth <alan.lanceloth@gmail.com>
2024-03-22 21:58:43 -0300

    update: refactor authenticate_google_calendar to use .env variables

--- src/calculate_page.py
+++ src/calculate_page.py
@@ -5,7 +5,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
-import os.path
+import os
 import pickle
 from datetime import datetime, timedelta
 from urllib.parse import unquote
@@ -24,10 +24,22 @@ def authenticate_google_calendar():
             creds.refresh(Request())
         else:
             # Modificado para carregar as credenciais da conta de serviço do Streamlit Cloud Secrets
-            service_account_info = st.secrets["google_calendar"]['var']
+            service_account_info = {
+              "type": "service_account",
+              "project_id": os.environ["GOOGLE_PROJECT_ID"],
+              "private_key_id": os.environ["GOOGLE_PRIVATE_KEY_ID"],
+              "private_key": os.environ["GOOGLE_PRIVATE_KEY"],
+              "client_email": os.environ["GOOGLE_CLIENT_EMAIL"],
+              "client_id": os.environ["GOOGLE_CLIENT_ID"],
+              "auth_uri": os.environ["GOOGLE_AUTH_URI"],
+              "token_uri": os.environ["GOOGLE_TOKEN_URI"],
+              "auth_provider_x509_cert_url": os.environ["GOOGLE_AUTH_PROVIDER_CERT_URL"],
+              "client_x509_cert_url": os.environ["GOOGLE_CLIENT_CERT_URL"]
+            }
             creds = Credentials.from_service_account_info(
                 service_account_info, scopes=SCOPES)
     return build('calendar', 'v3', credentials=creds)
+
 
 def send_calendar_event(username, exit_time):
     # Autentica e autoriza a aplicação
